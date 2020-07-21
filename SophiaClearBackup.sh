62d361b3ec3dab8bb8046b7a1682ba4f6ae1d13d
Adriano Ruseler <ruseler@utfpr.edu.br>
2020-07-21 15:43:42 -0300

    Create SophiaClearBackup.sh

---
+++ SophiaClearBackup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+DB_BKP="/mnt/nvme1n1p1/mdlbkp/db/" # moodle database backup folder
+DATA_BKP="/mnt/nvme1n1p1/mdlbkp/data/"  # moodle data backup folder
+HTML_BKP="/mnt/nvme1n1p1/mdlbkp/html/"  # moodle html backup folder
+AUTO_BKP="/mnt/nvme1n1p1/mdlbkp/auto/"  # moodle auto backup folder
+
+cd $DB_BKP
+
+ls -lh $DB_BKP
+rm -rf *.gz
+rm -rf *.gz.md5
+
+cd $DATA_BKP
+
+ls -lh $DATA_BKP
+rm -rf *.gz
+rm -rf *.gz.md5
+
+cd $HTML_BKP
+
+ls -lh $HTML_BKP
+rm -rf *.gz
+rm -rf *.gz.md5
+
+cd $AUTO_BKP
+
+ls -lh $AUTO_BKP
+rm -rf *.mbz
+
+
+
