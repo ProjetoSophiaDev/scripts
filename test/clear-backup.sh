17888ad33e5eb1d5ffd75f710297acfccb841ec1
Adriano Ruseler <ruseler@utfpr.edu.br>
2020-07-21 15:50:59 -0300

    Update clear-backup.sh

--- test/clear-backup.sh
+++ test/clear-backup.sh
@@ -2,7 +2,6 @@
 
 DB_BKP="/mnt/mdl/bkp/db/" # moodle database backup folder
 DATA_BKP="/mnt/mdl/bkp/data/" # moodle data backup folder
-HTML_BKP="/mnt/mdl/bkp/html/" # moodle html backup folder
 AUTO_BKP="/mnt/mdl/bkp/auto/"  # moodle auto backup folder
 
 cd $DB_BKP
@@ -14,12 +13,6 @@ rm -rf *.gz.md5
 cd $DATA_BKP
 
 ls -lh $DATA_BKP
-rm -rf *.gz
-rm -rf *.gz.md5
-
-cd $HTML_BKP
-
-ls -lh $HTML_BKP
 rm -rf *.gz
 rm -rf *.gz.md5
 
